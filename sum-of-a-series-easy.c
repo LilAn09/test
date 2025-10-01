3235f56e2370a852a874d5af180cd46b1b3270df
LilAn09 <1327161394@qq.com>
2025-10-01 23:21:54 +0800

    级数求和

---
+++ sum-of-a-series-easy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{   
+    int k,n=0;
+
+    double sum=0;
+
+    scanf("%d",&k);
+
+    while(sum<=k)
+    {
+        n++;
+        sum+=1.0/n;
+    }
+
+    printf("%d",n);
+
+   return 0;
+}
