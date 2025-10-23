1bfd284a04bd24977d1bcb96d2b19b8e3f439710
LilAn09 <1327161394@qq.com>
2025-10-23 17:06:30 +0800

    利润计算（优化版）

--- profit-easy.c
+++ profit-easy.c
@@ -4,21 +4,26 @@ int main()
 {   
     int profit = 0;
     int bonus = 0;
+    int bonus1 = 100000*0.1;
+    int bonus2 = bonus1+100000*0.075;
+    int bonus3 = bonus2+200000*0.05;
+    int bonus4 = bonus3+200000*0.03;
+    int bonus5 = bonus4+400000*0.015;
 
     scanf("%d",&profit);
 
     if(profit<=100000){
         bonus = profit*0.1;
     }else if(profit<=200000){
-        bonus = 100000*0.1+(profit-100000)*0.075;
+        bonus = bonus1+(profit-100000)*0.075;
     }else if(profit<=400000){
-        bonus = 100000*0.1+100000*0.075+(profit-200000)*0.05;
+        bonus = bonus2+(profit-200000)*0.05;
     }else if(profit<=600000){
-        bonus = 100000*0.1+100000*0.075+200000*0.05+(profit-400000)*0.03;
+        bonus = bonus3+(profit-400000)*0.03;
     }else if(profit<=1000000){
-        bonus = 100000*0.1+100000*0.075+200000*0.05+200000*0.03+(profit-600000)*0.015;
+        bonus = bonus4+(profit-600000)*0.015;
     }else{
-        bonus = 100000*0.1+100000*0.075+200000*0.05+200000*0.03+400000*0.015+(profit-1000000)*0.01;
+        bonus = bonus5+(profit-1000000)*0.01;
     }
     printf("%d\n",bonus);
 
